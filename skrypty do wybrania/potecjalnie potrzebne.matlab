a07dfbbd279a958dbbfc470d3342065d9ddee819
Marcin Grześkowiak <grzeskowiak.marcin@gmail.com>
2015-10-21 09:32:19 +0200

    Changesesese

---
+++ skrypty do wybrania/potecjalnie potrzebne.matlab
@@ -0,0 +1,30 @@
+przyciecie=[0,a/3,b,(a/3)*2]; %% zmiana przyciêcia obrazu!!!!!
+O_3=imcrop(O,przyciecie);
+Or=imresize(O_3,0.25); %% zmiana rozdzielczoœci przetwarzania
+Or_hsv=rgb2hsv(Or);
+
+
+P_300 = bwareaopen(R(eee,1).K1pr_auto, 50); %usuniêcie obiektów o mniejszej iloœci pikseli ni¿ 50
+P_WDZ = imfill(P_300, 'holes'); %wype³nienie dziur
+
+STATS = regionprops(P_WDZ, 'Orientation'); %obliczenie orientacji wszystkich pojedynczych obiektów
+IL=bwlabel(P_WDZ); %zlabelowanie wszystkich obiektów
+ind = find([STATS.Orientation] >= 45 | [STATS.Orientation] <= -45); %wybór obiektów o odpowiednim nachyleniu
+P_ODS = ismember(IL,ind); %odsiew
+STATS = regionprops(P_ODS, 'MajorAxisLength','MinorAxisLength'); %obliczenie przek¹tnych obiektów
+[a b]=size(STATS);
+if a>0
+        for i=1:a %dodanie informacji o stosunku boków
+            STATS(i,1).mimj=STATS(i,1).MinorAxisLength/STATS(i,1).MajorAxisLength; 
+        end
+        ind2 = find([STATS.mimj] > 0.05);
+        ILL=bwlabel(P_ODS);
+        P_ODS_2 = ismember(ILL,ind2); %odsiew chudzielców
+end
+
+ % sumowanie obrazów 1) i 2)
+if a>0
+    P_OST=P_DO+P_ODS_2;
+else
+    P_OST=P_DO;
+end
---
+++ skrypty do wybrania/r_4_test.m
@@ -0,0 +1,85 @@
+% autor: E.Pastucha klasyfikacja wykrytych znaków
+tic
+sciezka_wzorce= 'C:\Users\Marcin\Desktop\W11p\wzorce\';
+
+w1='W11_p_1_';
+w2='W11_p_2_';
+koncowka='.tif';
+wynik=fopen('WYNIK.txt','w');
+
+for eee=1:119
+    
+    if R(eee,1).t==1
+        [a b]=size(R(eee,1).TZ);
+        f=1;
+        x=0;
+        for i=1:a
+            if R(eee,1).TZ(i,1).bz==0
+                stat=regionprops(R(eee,1).TZ(i,1).KandydatZnak,'BoundingBox');
+                ZNAK=imcrop(R(eee,1).TZ(i,1).KandydatZnak,stat.BoundingBox);
+                [c d]=size(ZNAK);
+                k=1;
+                for j=30:-3:-30
+                    n=num2str(j);
+                    nazwa1=strcat(w1,n,koncowka);
+                    nazwa2=strcat(w2,n,koncowka);
+                    W1=imread([sciezka_wzorce nazwa1]);
+                    W2=imread([sciezka_wzorce nazwa2]);
+                    w_p1=imresize(W1,[c d]);
+                    w_p2=imresize(W2,[c d]);
+                    O_p1=ZNAK-w_p1;
+                    O_p2=ZNAK-w_p2;
+                    P_1(k,1)=j;
+                    P_1(k,2)=length(find(O_p1>0));
+                    P_1(k,3)=length(find(O_p1<0));
+                    P_1(k,4)=sqrt(((P_1(k,2))^2)+((P_1(k,3))^2));
+                    P_2(k,1)=j;
+                    P_2(k,2)=length(find(O_p2>0));
+                    P_2(k,3)=length(find(O_p2<0));
+                    P_2(k,4)=sqrt(((P_2(k,2))^2)+((P_2(k,3))^2));
+                    k=k+1;
+                end
+                min_p1=min(P_1(:,4));
+                min_p2=min(P_2(:,4));
+                p1=find([P_1(:,4)]==min_p1);
+                p2=find([P_2(:,4)]==min_p2);
+
+                if min_p1<min_p2
+                    R(eee,1).TZ(i,1).kat=P_1(p1,1);
+                    R(eee,1).TZ(i,1).n=w1;
+                    fprintf('Iteracja %d zdjêcie %s znaleziono znak P1\n', eee, R(eee,1).nazwa)
+                    fprintf(wynik,'Zdjêcie %s znaleziono wskaŸnik W11 p1\n',R(eee,1).nazwa);
+                    %fprintf(wynik,'Po³o¿enie na obrazie: lewy górny naro¿nik(%d,%d),szerokoœæ %d, wysokoœæ %d\n',Z(eee,1).Z(i,1).bb(1,1),Z(eee,1).Z(i,1).bb(1,2),Z(eee,1).Z(i,1).bb(1,3),Z(eee,1).Z(i,1).bb(1,4));
+                    fprintf(wynik,'\n');
+                else
+                    R(eee,1).TZ(i,1).kat=P_2(p2,1);
+                    R(eee,1).TZ(i,1).n=w2;
+                    fprintf('Iteracja %d zdjêcie %s znaleziono znak P2\n', eee, R(eee,1).nazwa)
+                    fprintf(wynik,'Zdjêcie %s znaleziono wskaŸnik W11 p2\n',R(eee,1).nazwa);
+                    %fprintf(wynik,'Po³o¿enie na obrazie: lewy górny naro¿nik(%d,%d),szerokoœæ %d, wysokoœæ %d\n',Z(eee,1).Z(i,1).bb(1,1),Z(eee,1).Z(i,1).bb(1,2),Z(eee,1).Z(i,1).bb(1,3),Z(eee,1).Z(i,1).bb(1,4));
+                    fprintf(wynik,'\n');
+                end
+                
+                ORYG=R(eee,1).O;
+                figure(eee);subplot(a,4,1+x);imshow(ORYG);
+                title('fragment obrazu oryginalnego');
+                K1=imcrop(R(eee,1).K1, R(eee,1).TZ(i,1).BB);
+                figure(eee);subplot(a,4,2+x);imshow(K1);
+                title('fragment odpowidzi wêz³a K1 sieci neuronowej');
+                figure(eee);subplot(a,4,3+x);imshow(ZNAK);
+                title('ostateczny obraz progowania po prostowaniu');
+                n=num2str(R(eee,1).TZ(i,1).kat);
+                nazwa=strcat(R(eee,1).TZ(i,1).n,n,koncowka);
+                S=imread([sciezka_wzorce nazwa]);
+                figure(eee);subplot(a,4,4+x);imshow(S);
+                title('najlepszy odpowiadaj¹cy wzorzec');
+                x=x+4;
+                %}
+            end
+        end
+        
+    end
+     
+end
+clearvars -except R
+toc  
